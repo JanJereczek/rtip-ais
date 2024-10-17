e88ea058996a8a46ea77e1ed25e588ad1d0bd1d9
jereczek <jereczek@pik-potsdam.de>
2024-10-17 14:48:18 +0200

    add netcdf and include all src files

--- scripts/intro.jl
+++ scripts/intro.jl
@@ -5,10 +5,11 @@ using CairoMakie
 using DelimitedFiles
 using Interpolations
 using NCDatasets
+using NetCDF
 using Statistics
 
 # Here you may include files from the source directory
-include(srcdir("dummy_src_file.jl"))
+[include(srcdir("$src_file")) for src_file in readdir(srcdir())]
 
 println(
 """
