b8719376586a7199faa639d9da6067c8d679a4ed
jereczek <jereczek@pik-potsdam.de>
2025-01-16 17:17:42 +0100

    improvement for restart debug

--- src/plot2D.jl
+++ src/plot2D.jl
@@ -1,4 +1,4 @@
-function plot_diffheatmaps(xp1, xp2, prefix)
+function plot_diffheatmaps(xp1, xp2, prefix; symmetric = true, tol = 1e-8)
     ds1 = NCDataset(xp1)
     ds2 = NCDataset(xp2)
     # iterate over all variables
@@ -6,13 +6,46 @@ function plot_diffheatmaps(xp1, xp2, prefix)
         if ndims(var) == 3
             fig = Figure(size = (900,800))
             ax = Axis(fig[1,1], aspect = DataAspect())
-            hm = heatmap!(ax, ds2[varname][:, :, 1] .- ds1[varname][:, :, end], colormap = :viridis)
+            diff = ds2[varname][:, :, 1] .- ds1[varname][:, :, end]
+            amp = maximum(abs.(diff))
+            if symmetric
+                cmap = (colormap = cgrad([:cornflowerblue, :white, :darkorange]),
+                    colorrange = (-amp-tol, amp+tol))
+            else
+                cmap = (colormap = :viridis, colorrange = extrema(diff))
+            end
+            hm = heatmap!(ax, diff; cmap...)
             Colorbar(fig[1,2], hm, label = "Δ $varname", height = Relative(0.5))
             save(plotsdir("diffheatmap/$prefix/$(varname).png"), fig)
         end
     end
     close(ds1)
     close(ds2)
+end
+
+function plot_diffheatmap(xp1, xp2, varname::String, prefix, crange)
+
+    fig = Figure(size = (450, 400))
+    ax = Axis(fig[1,1], aspect = DataAspect())
+    hidedecorations!(ax)
+    hm = plot_diffheatmap!(ax, xp1, xp2, varname, crange)
+    Colorbar(fig[1,2], hm, label = "Δ $varname", height = Relative(0.5))
+
+    isdir(plotsdir("diffheatmap/$prefix")) || mkdir(plotsdir("diffheatmap/$prefix"))
+    save(plotsdir("diffheatmap/$prefix/$(varname).png"), fig)
+end
+
+function plot_diffheatmap!(ax, xp1, xp2, varname::String, crange)
+    ds1 = NCDataset(xp1)
+    ds2 = NCDataset(xp2)
+
+    diff = ds2[varname][:, :, 1] .- ds1[varname][:, :, 1]
+    cmap = (colormap = cgrad([:cornflowerblue, :white, :darkorange]), colorrange = crange)
+
+    close(ds1)
+    close(ds2) 
+
+    return heatmap!(ax, diff; cmap...)
 end
 
 function plot_heatmaps(xp, path_to_folder, idx)
